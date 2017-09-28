bd98033847d6cfb350ccf5ddc94186670c273236
Faminchik Evgeniy <f.zhek@mail.ru>
2017-09-28 16:57:58 +0300

    fsm.js has been updated

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,50 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        this.config = config;
+        this.fsm = [null, this.config.initial];
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.fsm[this.fsm.length - 1];
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+            if (this.config.states.hasOwnProperty(state))
+                this.fsm.push(state);
+            else
+                throw new TypeError();
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        var flag = false;
+        if (Object.keys(this.config.states[this.fsm[this.fsm.length - 1]].transitions).includes(event)){
+            this.fsm.push(this.config.states[this.fsm[this.fsm.length - 1]].transitions[event]);
+            flag = true;
+        }
+        if (!flag)
+            throw new TypeError();
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.fsm.push(this.config.initial);
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,28 +54,54 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        if (arguments.length === 0)
+            return Object.keys(this.config.states);
+        var arr = [];
+        for (var st in this.config.states)
+            if (Object.keys(this.config.states[st].transitions).includes(event))
+                arr.push(st);
+        return arr;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if(this.fsm[this.fsm.length - 2] === null)
+            return false;
+        this.fsm.unshift(this.fsm[this.fsm.length - 1]);
+        this.fsm.splice(this.fsm.length - 1, 1);
+        return true;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        if(this.fsm[0] === null)
+            return false;
+        this.fsm.push(this.fsm[0]);
+        this.fsm.splice(0, 1);
+        return true;
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.fsm = [null, this.config.initial];
+    }
 }
 
 module.exports = FSM;
 
 /** @Created by Uladzimir Halushka **/
+
+
+
+
